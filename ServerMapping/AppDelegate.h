88bbc8cce2707f89e2c892ca8d8b83c50fd31b9a
XieTian <Stevens@MacBook-Air.local>
2017-08-02 16:38:40 +0800

    Update Code

---
+++ ServerMapping/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ServerMapping
+//
+//  Created by WhatsXie on 2017/8/2.
+//  Copyright © 2017年 StevenXie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ServerMapping/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ServerMapping
+//
+//  Created by WhatsXie on 2017/8/2.
+//  Copyright © 2017年 StevenXie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
